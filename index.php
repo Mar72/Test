7519898b02342aa98fa53de22a90b5523d1626cc
usuarim <jf.franco.css@gmail.com>
2020-02-27 19:58:58 +0100

    Versión 1.0.0.2 Corregida.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
   for ($i=0;$i<=10;$i++)
      echo "<p>Hola mundo</p>";   
-        echo "Hola Mar"; 
-        echo "Hola, José Francisco!?>
+  echo "Hola Mar"; 
+  echo "Hola, José Francisco";
+?>
